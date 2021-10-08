6e2531c4f78049af2aaad3e91115408f61118387
Joe Escobedo <joe_escob1023@live.com>
2021-10-08 14:22:40 -0700

    server is finally running

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,12 @@
 // build your `/api/projects` router here
+const router = require("express").Router();
+const Project = require("./model");
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: "something went wrong in the recipes router",
+    message: err.message,
+  });
+});
+
+module.exports = router;
